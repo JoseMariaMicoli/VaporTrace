5d9ca6412e19b4c80dd7a260b80ea46d5e152ecf
Xoce <josemaria.micoli@gmail.com>
2026-01-22 19:13:16 -0300

    Phase 8.2 completed
    (cherry picked from commit 0651687b9e5e9c0e8c2c41bfedabddf61e0ee07a)

--- pkg/logic/loot.go
+++ pkg/logic/loot.go
@@ -13,16 +13,20 @@ type Finding struct {
 }
 
 var (
-	Vault []Finding
+	Vault    []Finding
 	vaultMux sync.Mutex
 
 	// Automated PII & Secret Patterns
 	Patterns = map[string]*regexp.Regexp{
-		"AWS_KEY":     regexp.MustCompile(`AKIA[0-9A-Z]{16}`),
-		"JWT_TOKEN":   regexp.MustCompile(`eyJ[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*`),
-		"CREDIT_CARD": regexp.MustCompile(`(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12})`),
-		"EMAIL":       regexp.MustCompile(`[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`),
-		"SLACK_TOKEN": regexp.MustCompile(`xox[baprs]-[0-9a-zA-Z]{10,48}`),
+		"AWS_KEY":       regexp.MustCompile(`AKIA[0-9A-Z]{16}`),
+		"JWT_TOKEN":     regexp.MustCompile(`eyJ[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*`),
+		"CREDIT_CARD":   regexp.MustCompile(`(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12})`),
+		"EMAIL":         regexp.MustCompile(`[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`),
+		"SLACK_TOKEN":   regexp.MustCompile(`xox[baprs]-[0-9a-zA-Z]{10,48}`),
+		// Phase 8.2 Additions - Wrapped in MustCompile to fix type mismatch
+		"DB_CONNECTION": regexp.MustCompile(`(postgres|mysql|mongodb):\/\/[a-zA-Z0-9]+:[a-zA-Z0-9]+@[a-z0-9.-]+:\d+\/\w+`),
+		"STACK_TRACE":   regexp.MustCompile(`(?i)(stacktrace|exception|at\s+[\w\.]+\([\w\.]+\.java:\d+\)|File\s+".+\.py",\s+line\s+\d+)`),
+		"METADATA_IP":   regexp.MustCompile(`169\.254\.169\.254`),
 	}
 )
 
